087450cff465ee39406b8dc473e5eae001fc6717
Seong <sryoo6@gatech.edu>
2022-01-10 12:19:36 -0500

    Script and style constants

--- functions.php
+++ functions.php
@@ -1,10 +1,26 @@
 <?php
 
 class UrbanHeatATL_Theme {
+  const STYLES = array(
+    'fonts',
+    'base',
+    'utils',
+    'layout',
+    'text',
+  );
+  const SCRIPTS = array(
+
+  );
   function actions__general() {
     // Add type="module" to scripts
     add_filter( 'script_loader_tag', function( $tag, $handle, $src ) {
-      return '<script type="module" src="' . esc_url( $src ) . '"></script>';
+      $scripts = constant( __CLASS__ . "::SCRIPTS" );
+      foreach( $scripts as $script ) {
+        if ( $handle == 'urbanheat-' . $script ) {
+          return '<script type="module" src="' . esc_url( $src ) . '"></script>';
+        }
+      }    
+      return $tag;
     }, 10, 3 );
     // (Provided by Wordpress) Filter to make Wordpress 'Read More' button more accessible    
     add_filter( 'the_content_more_link', function( $html ) {
@@ -22,12 +38,7 @@ class UrbanHeatATL_Theme {
     // Stylesheets and JS files for front end (client view)
     add_action( 'wp_enqueue_scripts', function() {
       $style_url = get_template_directory_uri() . '/css/build/';
-      $styles = array(
-        'fonts',
-        'base',
-        'utils',
-        'layout',
-      );
+      $styles = constant( __CLASS__ . "::STYLES" );
       foreach ( $styles as $style ) {
         wp_enqueue_style(
           'urbanheat-' . $style,
