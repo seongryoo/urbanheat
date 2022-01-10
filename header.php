69e97c30f3b74d9fa8db4e3d09073e77b5772128
Seong <sryoo6@gatech.edu>
2022-01-10 11:50:41 -0500

    Initial commit, blank index php

---
+++ header.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html <?php language_attributes(); ?>>
+
+<head>
+  <meta charset="<?php bloginfo( 'charset' ); ?>">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <?php wp_head(); ?>
+</head>
+
+<body>
+
+<nav aria-labelledby="skiplink" class="contained">
+  <span id="skiplink" class="aria-labelledby">Skip link</span>
+  <a href="#main" class="skip-link">
+    Skip to main content
+  </a>
+</nav>
---
+++ index.php
@@ -0,0 +1,21 @@
+<?php get_header(); ?>
+
+<main id="main">
+  <?php if ( have_posts() ) {
+    while ( have_posts() ) {
+      the_post(); ?>
+      
+        <h1 class="heading--page-title contained">
+          <?php the_title(); ?>
+        </h1>
+
+        <div class="contained">
+          <?php the_content(); ?>
+        </div>
+      <?php
+    }
+  }
+  ?>
+</main>
+
+<?php get_footer(); ?>
