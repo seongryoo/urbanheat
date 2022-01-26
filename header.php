8fc206ceda31d4e2295e107d8d7b8551be6379a7
Seong <sryoo6@gatech.edu>
2022-01-25 22:07:00 -0500

    Scaffolding for possible custom menu generation

--- header.php
+++ header.php
@@ -9,9 +9,24 @@
 
 <body>
 
-<nav aria-labelledby="skiplink" class="contained">
-  <span id="skiplink" class="aria-labelledby">Skip link</span>
+<nav aria-labelledby="label_skiplink" class="contained">
+  <span id="label_skiplink" class="aria-labelledby" aria-hidden="true">
+    <?php _e( 'Skip link', 'urbanheat' ); ?>
+  </span>
   <a href="#main" class="skip-link">
     <?php _e( 'Skip to main content', 'urbanheat' ); ?>
   </a>
+</nav>
+
+<nav aria-labelledby="label_mainnav">
+  <span id="label_mainnav" class="aria-labelledby" aria-hidden="true">
+    <?php _e( 'Main menu', 'urbanheat' ); ?>
+  </span>
+  <?php 
+  // Using a custom nav walker in order to generate a more accessible nested menu
+    wp_nav_menu( array(
+      'menu' => 'primary',
+      'walker' => new UrbanHeatATL_Nav_Walker()
+    ) );
+  ?>
 </nav>
---
+++ theme-includes/class-menu-walker.php
@@ -0,0 +1,7 @@
+<?php
+// Custom walker generates more accessible nested menu
+// Parent menu items are turned into buttons which can be toggled
+// Single menu items remain as page links
+class UrbanHeatATL_Nav_Walker extends Walker_Nav_Menu {
+
+}
