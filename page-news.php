4f2087bdea38c1001af16295b5c4b6c8f831253e
Seong <--global>
2022-03-20 17:02:18 -0400

    Image and video stylings

---
+++ page-news.php
@@ -0,0 +1,22 @@
+<?php get_header(); ?>
+
+<main id="main">
+  <?php if ( have_posts() ) {
+    while ( have_posts() ) {
+      the_post(); ?>
+      
+
+        <div class="contained">
+          <h1 class="heading--page-title">
+            <?php the_title(); ?>
+          </h1>
+          <?php the_content(); ?>
+          <p>Welcomet o the news</p>
+        </div>
+      <?php
+    }
+  }
+  ?>
+</main>
+
+<?php get_footer(); ?>
